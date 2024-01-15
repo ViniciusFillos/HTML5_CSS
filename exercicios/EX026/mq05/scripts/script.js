8d0688a79e96891bdbf5b16508b37bf2629c79cf
Vinicius Ruan Fillos <107576816+ViniciusFillos@users.noreply.github.com>
2024-01-15 16:35:45 -0300

    burger menu

---
+++ exercicios/EX026/mq05/scripts/script.js
@@ -0,0 +1,10 @@
+function burger() {
+
+    let itens = document.querySelector(`menu`)
+    
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
